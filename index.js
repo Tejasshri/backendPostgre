9dccb729d247461d4dbd8820a824b38914ecd776
tejasshri <tejasshrishekhar08@gmail.com>
2023-09-30 03:10:23 +0530

    update/add bookmark table

--- index.js
+++ index.js
@@ -78,3 +78,52 @@ app.put("/todo/:id", async (request, response) => {
   await db.run(query);
   response.send({ statusCode: 200, msg: "Updated Successfully" });
 });
+
+app.get("/bookmark", async (request, response) => {
+  try {
+    const query = `
+      SELECT 
+        id,
+        web_logo as webLogo,
+        url,
+        display_text as displayText
+      FROM bookmark ;
+    `;
+    const bookMarkList = await db.all(query);
+    response.send(bookMarkList);
+  } catch (error) {
+    console.log(`Error occured in API: ${error}`);
+  }
+});
+
+app.post("/bookmark", async (request, response) => {
+  const { webLogo = "", url = "", displayText = "" } = request.body;
+  console.log(webLogo, url, displayText);
+  const check = await db.get(`
+    SELECT * FROM bookmark
+    WHERE url = '${url}' ;
+  `);
+  if (check === undefined) {
+    const query = ` 
+        INSERT INTO bookmark 
+          (web_logo, url, display_text)
+        VALUES
+          ('${webLogo}', '${url}', '${displayText}') ;
+      `;
+    const data = await db.run(query);
+    response.send({ statusCode: 200, msg: "Bookmark added successfully" });
+  } else {
+    response.status(400);
+    response.send({ statusCode: 400, msg: "Already exist" });
+  }
+});
+
+app.delete("/bookmark/del/:id", async (request, response) => {
+  const { id } = request.params;
+  const query = ` 
+        DELETE FROM bookmark
+        WHERE id = ${id} ;
+      `;
+  const data = await db.run(query);
+  response.send({ statusCode: 200, msg: "Deleted Successfully" });
+});
