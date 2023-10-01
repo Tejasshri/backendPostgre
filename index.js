e73a25fb7d0ab6360f19385c9c348bff9692aa38
tejasshri <tejasshrishekhar08@gmail.com>
2023-10-01 20:02:00 +0530

    authentication added

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const { open } = require("sqlite");
 const sqlite3 = require("sqlite3");
 const cors = require("cors");
 const path = require("path");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
 const { request } = require("http");
 const { log } = require("console");
 
@@ -28,6 +31,35 @@ const initilizeDBAndServer = async () => {
 };
 
 initilizeDBAndServer();
+
+const userAuthentication = (request, response, next) => {
+  try {
+    const authorization = request.headers.authorization;
+    console.log({ authorization });
+    let token;
+    if (authorization !== undefined) {
+      token = authorization.split(" ")[1];
+    }
+    if (token === undefined) {
+      console.log("Triggered", token);
+      response.status(400);
+      response.send({ msg: "Invalid Token" });
+    } else {
+      jwt.verify(token, "XXYY", (err, payload) => {
+        if (err) {
+          response.status(400);
+          response.send({ msg: "Invalid Token" });
+        } else {
+          request.userId = payload.user_id;
+          request.username = payload.username;
+          next();
+        }
+      });
+    }
+  } catch (error) {
+    console.log(`Error occured in Middleware: ${error}`);
+  }
+};
 
 app.post("/", async (request, response) => {
   try {
@@ -79,7 +111,7 @@ app.put("/todo/:id", async (request, response) => {
   response.send({ statusCode: 200, msg: "Updated Successfully" });
 });
 
-app.get("/bookmark", async (request, response) => {
+app.get("/bookmark", userAuthentication, async (request, response) => {
   try {
     const query = `
       SELECT 
@@ -96,7 +128,7 @@ app.get("/bookmark", async (request, response) => {
   }
 });
 
-app.post("/bookmark", async (request, response) => {
+app.post("/bookmark", userAuthentication, async (request, response) => {
   const { webLogo = "", url = "", displayText = "" } = request.body;
   console.log(webLogo, url, displayText);
   const check = await db.get(`
@@ -113,17 +145,100 @@ app.post("/bookmark", async (request, response) => {
     const data = await db.run(query);
     response.send({ statusCode: 200, msg: "Bookmark added successfully" });
   } else {
-    response.status(400);
+    // esponse.status(400);
     response.send({ statusCode: 400, msg: "Already exist" });
   }
 });
 
-app.delete("/bookmark/del/:id", async (request, response) => {
-  const { id } = request.params;
-  const query = ` 
+app.delete(
+  "/bookmark/del/:id",
+  userAuthentication,
+  async (request, response) => {
+    const { id } = request.params;
+    const query = ` 
         DELETE FROM bookmark
         WHERE id = ${id} ;
       `;
-  const data = await db.run(query);
-  response.send({ statusCode: 200, msg: "Deleted Successfully" });
+    const data = await db.run(query);
+    response.send({ statusCode: 200, msg: "Deleted Successfully" });
+  }
+);
+
+app.post("/register", async (request, response) => {
+  setTimeout(() => {
+    console.clear();
+  }, 15000);
+  try {
+    const { username, password, mobileNo, dob } = request.body;
+    console.log(username, password, mobileNo, dob);
+    const checkQuery = `
+      SELECT * FROM user 
+      WHERE username = '${username}' OR mobile_no = ${mobileNo}
+    `;
+    const dbUser = await db.get(checkQuery);
+    if (dbUser === undefined) {
+      if (password.length < 7) {
+        response.status(400);
+        response.send({ msg: "Password is too short" });
+      } else if (String(mobileNo).length !== 10) {
+        response.status(400);
+        response.send({ msg: "Not a valid Number" });
+      } else {
+        const hashPassword = await bcrypt.hash(password, 10);
+        const query = `
+          INSERT INTO user 
+            (username, password, mobile_no, dob) 
+          VALUES
+            ('${username}', '${hashPassword}', ${mobileNo}, '${dob}') ;
+        `;
+        const dbResponse = await db.run(query);
+        const lastId = dbResponse.lastID;
+        response.send({
+          msg: "Registeded Successfully",
+        });
+      }
+    } else {
+      const checkNoQuery = `select * from user WHERE mobile_no = ${mobileNo}`;
+      const result = await db.get(checkNoQuery);
+      if (result !== undefined) {
+        response.status(400);
+        response.send({ msg: "Mobile number registed already try another" });
+      } else {
+        response.status(400);
+        response.send({ msg: "Username already exists" });
+      }
+    }
+  } catch (error) {
+    console.log("Error occured in API: ", error);
+  }
+});
+
+app.post("/login", async (request, response) => {
+  try {
+    const { username, password } = request.body;
+    const query = `select * from user WHERE username = '${username}' `;
+    const result = await db.get(query);
+    if (result !== undefined) {
+      const isPasswordMatched = await bcrypt.compare(password, result.password);
+      if (isPasswordMatched) {
+        const payload = { username: result.username, user_id: result.id };
+        const jwtToken = jwt.sign(payload, "XXYY");
+        response.send({ jwtToken });
+      } else {
+        response.status(400);
+        response.send({ msg: "Wrong Password" });
+      }
+    } else {
+      response.status(400);
+      response.send({ msg: "Invalid Username" });
+    }
+  } catch (error) {
+    console.log(`Error occured in API: ${error}`);
+  }
+});
+
+app.get("/check/pass/my", userAuthentication, async (request, response) => {
+  const query = `select * from user;`;
+  const dbResponse = await db.all(query);
+  response.send(dbResponse);
 });
